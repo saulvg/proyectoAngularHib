01ed34f65427cfa42245b0d4103b79dc88223132
Saul Vaquero Garvin <saulvg@MacBook-Air-de-Saul.local>
2023-06-07 08:00:00 +0200

    s episodios

--- proyectoAngular/src/app/paginasDinamicas/episodios/paginas/publica/publica-episodios.component.ts
+++ proyectoAngular/src/app/paginasDinamicas/episodios/paginas/publica/publica-episodios.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Episodios } from '../../interfaces/episodios';
+import { Observable } from 'rxjs';
+import { ServicioEpisodioService } from '../../servicios/servicio-episodio.service';
 
 @Component({
   selector: 'app-publica-episodios',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class PublicaEpisodiosComponent {
 
+  //episodios:Episodios[];
+
+  episodios$ = new Observable<Episodios>
+
+  constructor(private srvEpisodios:ServicioEpisodioService){}
+ /*    this.episodios = [
+      {
+        id: 1,
+        titulo: "Pilot",
+        fechaEmision: "December 2, 2013",
+        episodio: "S01E01",
+        sinopsis: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores. Dale a Fistrum para que te salga ese pedaso de texto Chiquito en estado puro. Si te crees muy moderno dale a Latin que te lo pongo con cuarto y mitad de romanooo... Jarl!!"
+    },
+    {
+        id: 11,
+        titulo: "Ricksy Business",
+        fechaEmision: "April 14, 2014",
+        episodio: "S01E11",
+        sinopsis: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores. Dale a Fistrum para que te salga ese pedaso de texto Chiquito en estado puro. Si te crees muy moderno dale a Latin que te lo pongo con cuarto y mitad de romanooo... Jarl!!"
+    },
+    {
+        id: 25,
+        titulo: "Vindicators 3: The Return of Worldender",
+        fechaEmision: "August 13, 2017",
+        episodio: "S03E04",
+        sinopsis: "Bienvenido al Chiquito Ipsum, el generador de texto de relleno para tus diseños de antes de los dolores. Dale a Fistrum para que te salga ese pedaso de texto Chiquito en estado puro. Si te crees muy moderno dale a Latin que te lo pongo con cuarto y mitad de romanooo... Jarl!!"
+    }
+    ]
+  } */
+
+
+
+obtenrEpisodios(){
+  this.episodios$ = this.srvEpisodios.getEpisodios();
+}
+ngOnInit(){
+  this.obtenrEpisodios();
+}
 }
--- proyectoAngular/src/app/paginasDinamicas/episodios/servicios/servicio-episodio.service.ts
+++ proyectoAngular/src/app/paginasDinamicas/episodios/servicios/servicio-episodio.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EpisodiosDTO } from '../DTOs/episodiosDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioEpisodioService {
 
-  constructor() { }
+  private urlApi = "http://localhost:3000/";
+
+  constructor(private http:HttpClient) { }
+
+  getEpisodios():Observable<any>{
+    const url = "episodios";
+    return this.http.get<any>(this.urlApi+url)
+  }
 }
