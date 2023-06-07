e129100ed4e65a1df7d1e5112d288548eef92f2b
saul vaquero <saulvgproyecto@gmail.com>
2023-06-07 12:13:53 +0200

    publica episodios and img-rota

--- proyectoAngular/src/app/compartido/directivas/directivas.module.ts
+++ proyectoAngular/src/app/compartido/directivas/directivas.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MostrarCrudDirective } from './mostrar-crud.directive';
 import { ListadoDirective } from './listado.directive';
 import { LeerMasDirective } from './leer-mas.directive';
+import { ImgRotaDirective } from './img-rota.directive';
 
 
 
@@ -11,6 +12,7 @@ import { LeerMasDirective } from './leer-mas.directive';
     MostrarCrudDirective,
     ListadoDirective,
     LeerMasDirective,
+    ImgRotaDirective,
 
   ],
   imports: [
